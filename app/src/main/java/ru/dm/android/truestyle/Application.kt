3ca506069fc938aeacc13958e55f066b31365f97
Pavel-Team <Mevedor-Team@gmail.com>
2022-03-02 09:01:36 +0300

    first commit

---
+++ app/src/main/java/ru/dm/android/truestyle/Application.kt
@@ -0,0 +1,12 @@
+/**Класс приложения*/
+package ru.dm.android.truestyle
+
+import android.app.Application
+
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/ru/dm/android/truestyle/ui/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+/**Главная активити*/
+package ru.dm.android.truestyle.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.dm.android.truestyle.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    
+}
